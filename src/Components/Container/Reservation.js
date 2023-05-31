df492c4b41d841374719a725235568757fbee792
safaezouadi2023 <safae.zouadi2023@gmail.com>
2023-05-31 09:15:50 +0100

    Before the last update2

--- src/Components/Container/Reservation.js
+++ src/Components/Container/Reservation.js
@@ -1,9 +1,8 @@
 import React, { useState } from 'react';
-import Swal from 'sweetalert2';
-import { useNavigate } from 'react-router-dom';
+import swal from 'sweetalert';
+import { useNavigate, useParams } from 'react-router-dom';
 import './Reservation.css';
-import { useParams } from 'react-router-dom';
-import {FaPhone , FaEnvelope , FaBuilding } from "react-icons/fa";
+import { FaPhone, FaEnvelope, FaBuilding } from "react-icons/fa";
 
 function Reservation() {
   const [Vdepart, setVdepart] = useState('');
@@ -14,19 +13,22 @@ function Reservation() {
   const [Enfants, setEnfants] = useState('');
   const [Categorie, setCategorie] = useState('');
 
-  const Nave=useNavigate();
-  
-  const { selectedValue0,selectedValue1 } = useParams();
-  console.log(selectedValue0,selectedValue1);
+  const Nave = useNavigate();
+
+  const { selectedValue0, selectedValue1 } = useParams();
+  console.log(selectedValue0, selectedValue1);
+
   const successAlert = () => {
-    Swal.fire({
-      title: 'Bien Reserver',
-      icon: 'success'
-    });
+    swal( "Success","Bien Reserver", "success");
+  };
+
+  const errorAlert = () => {
+    swal("Error!", "You have to fill all the fields!", "error");
+
   };
 
   const Reserver = async (event) => {
-    event.preventDefault(); 
+    event.preventDefault();
     setVdepart('');
     setVarrivee('');
     setDdepart('');
@@ -51,132 +53,129 @@ function Reservation() {
         body: formData
       });
       if (result.ok) {
-        
         successAlert();
+        Nave("/");
       } else {
-        alert('Reservation submission failed.');
+        errorAlert();
       }
     } catch (error) {
       alert(`Reservation submission failed with error: ${error.message}`);
     }
 
-    Nave("/");
+    
   };
-
-
-
-
-
 
   return (
     <div className="Reservation">
-       
-          <div className='second-page-heading'>         
-          <h4>Book Prefered Deal Here</h4>
-          <h2>Make Your Reservation</h2>
+      <div className='second-page-heading'>
+        <h4>Book Preferred Deal Here</h4>
+        <h2>Make Your Reservation</h2>
+      </div>
+      <div className="more-info reservation-info">
+        <div className="container">
+          <div className="row">
+            <div className="col-lg-4 col-sm-6">
+              <div className="info-item">
+                <FaPhone id='icon' />
+                <h4>Make a Phone Call</h4>
+                <a href="tel:+212456789123">+212 456 789 123</a>
+              </div>
+            </div>
+            <div className="col-lg-4 col-sm-6">
+              <div className="info-item">
+                <FaEnvelope id='icon' />
+                <h4>Contact Us via Email</h4>
+                <a href="mailto:company@email.com">Email</a>
+              </div>
+            </div>
+            <div className="col-lg-4 col-sm-6">
+              <div className="info-item">
+                <FaBuilding id='icon' />
+                <h4>Visit Our Offices</h4>
+                <a href="00">24th Street North Avenue Tanger, Morocco</a>
+              </div>
+            </div>
           </div>
-          <div className="more-info reservation-info">
-    
-    
-    <div className="container">
-      <div className="row">
-        <div className="col-lg-4 col-sm-6">
-          <div className="info-item">
-            <FaPhone id='icon'/>
-            <h4>Make a Phone Call</h4>
-            <a href="00">+212 456 789 123</a>
-          </div>
-        </div>
-        <div class="col-lg-4 col-sm-6">
-          <div class="info-item">
-            <FaEnvelope  id='icon'/>
-            <h4>Contact Us via Email</h4>
-            <a href="00">company@email.com</a>
-          </div>
-        </div>
-        <div class="col-lg-4 col-sm-6">
-          <div class="info-item">
-            <FaBuilding  id='icon'/>
-            <h4>Visit Our Offices</h4>
-            <a href="00">24th Street North Avenue Tanger, Morroco</a>
-          </div>
+
         </div>
       </div>
-    </div>
+
+
+
+      <div className='form'>
+  <div className='faire'>
+    <h4>Faire Ton <em>Reservation</em> Par ce <em>formulaire</em></h4>
   </div>
 
-  <div className='form'>
-
-     <div className='faire'>
-          <h4>Faire Ton <em>Reservation</em> Par ce <em>formulaire</em></h4>
-     </div>
-
-  <form >
+  <form>
     <div className='row'>
-    <div className='col-lg-6'>
-          <fieldset>
+      <div className='col-lg-6'>
+        <fieldset>
           <label htmlFor="provenance">Ville de départ</label>
-                  <select className='inputR' value={Vdepart} onChange={(e)=>setVdepart(e.target.value)} >
-                      <option selected="selected">{selectedValue0}</option>
-                  </select>
-         </fieldset>
-    </div>
- 
-    <div className='col-lg-6'>  
-          <fieldset>      
-          <label htmlFor="arrivee">Ville d'arrivée</label>
-                  <select className='inputR' value={Varrivee} onChange={(e)=>setVarrivee(e.target.value)}>
-                      <option selected="selected">{selectedValue1}</option>
-                      <option>Paris</option>
-                      <option>Berne</option>
-                      <option>Dortmund</option>
-                  </select>
-           </fieldset>        
-    </div>
-    <div className='col-lg-6'>
-          <label htmlFor="depart">Date de départ</label>
-          <input type="date"  id="depart"  value={Ddepart} onChange={(e)=>setDdepart(e.target.value)} />
-    </div>
-    <div className='col-lg-6'>
-          <label htmlFor="retour">Date de Retour</label>
-          <input type="date" id="retour"   value={Dretour} onChange={(e)=>setDretour(e.target.value)} />
-    </div> 
+          <select className='inputR' value={Vdepart} onChange={(e) => setVdepart(e.target.value)}>
+            <option selected="selected">{selectedValue0}</option>
+            <option>Tanger</option>
+          </select>
+        </fieldset>
+      </div>
 
-    <div className='col-lg-6'>
-          <label htmlFor="adultes">Nombre d'adultes</label>
-          <select  id="adultes" value={Adultes} onChange={(e)=>setAdultes(e.target.value)}>
+      <div className='col-lg-6'>
+        <fieldset>
+          <label htmlFor="arrivee">Ville d'arrivée</label>
+          <select className='inputR' value={Varrivee} onChange={(e) => setVarrivee(e.target.value)}>
+            <option selected="selected">{selectedValue1}</option>
+            <option>Paris</option>
+            <option>Berne</option>
+            <option>Dortmund</option>
+          </select>
+        </fieldset>
+      </div>
+      <div className='col-lg-6'>
+        <label htmlFor="depart">Date de départ</label>
+        <input type="date" id="depart" value={Ddepart} onChange={(e) => setDdepart(e.target.value)} />
+      </div>
+      <div className='col-lg-6'>
+        <label htmlFor="retour">Date de Retour</label>
+        <input type="date" id="retour" value={Dretour} onChange={(e) => setDretour(e.target.value)} />
+      </div>
+
+      <div className='col-lg-6'>
+        <label htmlFor="adultes">Nombre d'adultes</label>
+        <select id="adultes" value={Adultes} onChange={(e) => setAdultes(e.target.value)}>
           <option selected="selected">Selectionner Votre Choix</option>
-            <option>1 </option>
-            <option>2 </option>
-            <option>3 </option>
-            <option>4 </option>
-            <option>5 </option>
-          </select>
-    </div>
-    <div className='col-lg-6'>
-          <label htmlFor="enfants">Nombre d'enfants</label>
-          <select  id="enfants" value={Enfants} onChange={(e)=>setEnfants(e.target.value)}>
+          <option>1</option>
+          <option>2</option>
+          <option>3</option>
+          <option>4</option>
+          <option>5</option>
+        </select>
+      </div>
+      <div className='col-lg-6'>
+        <label htmlFor="enfants">Nombre d'enfants</label>
+        <select id="enfants" value={Enfants} onChange={(e) => setEnfants(e.target.value)}>
           <option selected="selected">Selectionner Votre Choix</option>
-            <option>0</option>
-            <option>1</option>
-            <option>2</option>
-            <option>3</option>
-            <option>4</option>
-            <option>5</option>            
-          </select>
-    </div>
-    <div className='col-lg-12'>
-          <label htmlFor="type">Categorie</label>
-          <select  id="type" value={Categorie} onChange={(e)=>setCategorie(e.target.value)}>
-            <option selected="selected">Selectionner Votre Choix</option>
-            <option>Classe Economique</option>
-            <option>Classe VIP</option>
-          </select>
-    </div>
-    <div className='col-lg-12 ms-12'>
-              <div className="button-submit">
-                 <button type='button' id="recherche" onClick={Reserver}><span>Reserver</span></button>
-              </div>
+          <option>0</option>
+          <option>1</option>
+          <option>2</option>
+          <option>3</option>
+          <option>4</option>
+          <option>5</option>
+        </select>
+      </div>
+      <div className='col-lg-12'>
+        <label htmlFor="type">Categorie</label>
+        <select id="type" value={Categorie} onChange={(e) => setCategorie(e.target.value)}>
+          <option selected="selected">Selectionner Votre Choix</option>
+          <option>Classe Economique</option>
+          <option>Classe VIP</option>
+        </select>
+      </div>
+      <div className='col-lg-12 ms-12'>
+        <div className="button-submit">
+          <button type='button' id="recherche" onClick={Reserver}><span>Reserver</span></button>
+        </div>
+     
+
         </div>
       </div>
     </form>
